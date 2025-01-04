e3939bf3a65f889ea02f899e6189f9b91153aabb
Vivienne126 <vivienne.singh@cns.ac.in>
2025-01-04 11:42:48 +0530

    New3

---
+++ Activity3.py
@@ -0,0 +1,8 @@
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+sum=total_number*mean1
+new_sum=sum-wrong_number+correct_number
+new_mean=new_sum/total_number
+print("The new mean is ", new_mean)
