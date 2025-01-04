5a22e1684f05459d23de50e8cb60e6ec9d19ae41
Vivienne126 <vivienne.singh@cns.ac.in>
2025-01-04 11:25:59 +0530

    New2

---
+++ Activity2.py
@@ -0,0 +1,6 @@
+niumerator=int(input("Enter any Numerator"))
+denominator=int(input("Enter any denominator"))
+if niumerator%denominator == 0:
+    print("Divisible")
+else:
+    print("Not divisible")
